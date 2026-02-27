f562030dbd1df993ed10f15cbf79dd774511db70
florianthiery <mail@fthiery.de>
2026-02-27 15:39:36 +0000

    Deploying to gh-pages from @ Research-Squirrel-Engineers/EPICA-SISAL-FAIRification@4e19331be4cd89e5796228084dbb3cff5c7fc8b7 🚀

--- minivowl_result.js
+++ minivowl_result.js
@@ -6,11 +6,6 @@ var minivowlresult={
  ],
  "nodes": [
   {
-   "name": "Cave",
-   "type": "class",
-   "uri": "http://w3id.org/geo-lod/Cave"
-  },
-  {
    "name": "Delta13CSpeleothemObservation",
    "type": "class",
    "uri": "http://w3id.org/geo-lod/Delta13CSpeleothemObservation"
@@ -21,9 +16,19 @@ var minivowlresult={
    "uri": "http://w3id.org/geo-lod/Delta18OSpeleothemObservation"
   },
   {
-   "name": "Point",
+   "name": "Cave",
    "type": "class",
-   "uri": "http://www.opengis.net/ont/sf#Point"
+   "uri": "http://w3id.org/geo-lod/Cave"
+  },
+  {
+   "name": "E27_Site",
+   "type": "class",
+   "uri": "http://www.cidoc-crm.org/cidoc-crm/E27_Site"
+  },
+  {
+   "name": "E53_Place",
+   "type": "class",
+   "uri": "http://www.cidoc-crm.org/cidoc-crm/E53_Place"
   },
   {
    "name": "Speleothem",
@@ -31,24 +36,19 @@ var minivowlresult={
    "uri": "http://w3id.org/geo-lod/Speleothem"
   },
   {
-   "name": "UThChronology",
-   "type": "class",
-   "uri": "http://w3id.org/geo-lod/UThChronology"
-  },
-  {
    "name": "Delta13CProperty",
    "type": "class",
    "uri": "http://w3id.org/geo-lod/Delta13CProperty"
   },
   {
-   "name": "MeasurementType",
-   "type": "class",
-   "uri": "http://w3id.org/geo-lod/MeasurementType"
-  },
-  {
    "name": "Delta18OProperty",
    "type": "class",
    "uri": "http://w3id.org/geo-lod/Delta18OProperty"
+  },
+  {
+   "name": "MeasurementType",
+   "type": "class",
+   "uri": "http://w3id.org/geo-lod/MeasurementType"
   },
   {
    "name": "RollingMedianFilter",
@@ -61,279 +61,216 @@ var minivowlresult={
    "uri": "http://w3id.org/geo-lod/SavitzkyGolayFilter"
   },
   {
+   "name": "UThChronology",
+   "type": "class",
+   "uri": "http://w3id.org/geo-lod/UThChronology"
+  },
+  {
    "name": "Collection",
    "type": "class",
    "uri": "http://www.w3.org/2004/02/skos/core#Collection"
-  },
-  {
-   "name": "GeometryCollection",
-   "type": "class",
-   "uri": "http://www.opengis.net/ont/geosparql#GeometryCollection"
   }
  ],
  "links": [
   {
    "source": 1,
-   "target": 4,
-   "valueTo": "hasFeatureOfInterest",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/sosa/hasFeatureOfInterest"
-  },
-  {
-   "source": 2,
-   "target": 4,
-   "valueTo": "hasFeatureOfInterest",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/sosa/hasFeatureOfInterest"
-  },
-  {
-   "source": 1,
-   "target": 10,
-   "valueTo": "smoothingMethod_savgol",
-   "propertyTo": "class",
-   "uriTo": "http://w3id.org/geo-lod/smoothingMethod_savgol"
-  },
-  {
-   "source": 2,
-   "target": 10,
-   "valueTo": "smoothingMethod_savgol",
-   "propertyTo": "class",
-   "uriTo": "http://w3id.org/geo-lod/smoothingMethod_savgol"
-  },
-  {
-   "source": 1,
-   "target": 9,
-   "valueTo": "smoothingMethod_median",
-   "propertyTo": "class",
-   "uriTo": "http://w3id.org/geo-lod/smoothingMethod_median"
-  },
-  {
-   "source": 2,
-   "target": 9,
-   "valueTo": "smoothingMethod_median",
-   "propertyTo": "class",
-   "uriTo": "http://w3id.org/geo-lod/smoothingMethod_median"
-  },
-  {
-   "source": 1,
    "target": 8,
-   "valueTo": "observedProperty",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/sosa/observedProperty"
-  },
-  {
-   "source": 1,
-   "target": 6,
-   "valueTo": "observedProperty",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/sosa/observedProperty"
-  },
-  {
-   "source": 2,
-   "target": 8,
-   "valueTo": "observedProperty",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/sosa/observedProperty"
-  },
-  {
-   "source": 2,
-   "target": 6,
-   "valueTo": "observedProperty",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/ns/sosa/observedProperty"
-  },
-  {
-   "source": 1,
-   "target": 7,
    "valueTo": "measurementType",
    "propertyTo": "class",
    "uriTo": "http://w3id.org/geo-lod/measurementType"
-  },
-  {
-   "source": 2,
-   "target": 7,
-   "valueTo": "measurementType",
-   "propertyTo": "class",
-   "uriTo": "http://w3id.org/geo-lod/measurementType"
-  },
-  {
-   "source": 12,
-   "target": 0,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 12,
-   "target": 9,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 12,
-   "target": 4,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 12,
-   "target": 10,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 12,
-   "target": 7,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 12,
-   "target": 3,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 12,
-   "target": 6,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 12,
-   "target": 1,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 12,
-   "target": 8,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 12,
-   "target": 2,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 12,
-   "target": 5,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 11,
-   "target": 0,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 11,
-   "target": 9,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 11,
-   "target": 4,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 11,
-   "target": 10,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 11,
-   "target": 7,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 11,
-   "target": 3,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 11,
-   "target": 6,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 11,
-   "target": 1,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 11,
-   "target": 8,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 11,
-   "target": 2,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
-  },
-  {
-   "source": 11,
-   "target": 5,
-   "valueTo": "member",
-   "propertyTo": "class",
-   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
   },
   {
    "source": 0,
-   "target": 3,
-   "valueTo": "hasGeometry",
+   "target": 8,
+   "valueTo": "measurementType",
    "propertyTo": "class",
-   "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
+   "uriTo": "http://w3id.org/geo-lod/measurementType"
+  },
+  {
+   "source": 1,
+   "target": 9,
+   "valueTo": "smoothingMethod_median",
+   "propertyTo": "class",
+   "uriTo": "http://w3id.org/geo-lod/smoothingMethod_median"
+  },
+  {
+   "source": 0,
+   "target": 9,
+   "valueTo": "smoothingMethod_median",
+   "propertyTo": "class",
+   "uriTo": "http://w3id.org/geo-lod/smoothingMethod_median"
   },
   {
    "source": 1,
    "target": 5,
-   "valueTo": "ageChronologySpeleothem",
+   "valueTo": "hasFeatureOfInterest",
    "propertyTo": "class",
-   "uriTo": "http://w3id.org/geo-lod/ageChronologySpeleothem"
+   "uriTo": "http://www.w3.org/ns/sosa/hasFeatureOfInterest"
   },
   {
-   "source": 2,
+   "source": 0,
    "target": 5,
+   "valueTo": "hasFeatureOfInterest",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/sosa/hasFeatureOfInterest"
+  },
+  {
+   "source": 1,
+   "target": 11,
    "valueTo": "ageChronologySpeleothem",
    "propertyTo": "class",
    "uriTo": "http://w3id.org/geo-lod/ageChronologySpeleothem"
   },
   {
-   "source": 4,
+   "source": 0,
+   "target": 11,
+   "valueTo": "ageChronologySpeleothem",
+   "propertyTo": "class",
+   "uriTo": "http://w3id.org/geo-lod/ageChronologySpeleothem"
+  },
+  {
+   "source": 12,
+   "target": 5,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 12,
+   "target": 1,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 12,
+   "target": 4,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 12,
+   "target": 8,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 12,
+   "target": 10,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 12,
+   "target": 2,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 12,
+   "target": 7,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 12,
    "target": 0,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 12,
+   "target": 11,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 12,
+   "target": 9,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 12,
+   "target": 3,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 12,
+   "target": 6,
+   "valueTo": "member",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
+  },
+  {
+   "source": 1,
+   "target": 10,
+   "valueTo": "smoothingMethod_savgol",
+   "propertyTo": "class",
+   "uriTo": "http://w3id.org/geo-lod/smoothingMethod_savgol"
+  },
+  {
+   "source": 0,
+   "target": 10,
+   "valueTo": "smoothingMethod_savgol",
+   "propertyTo": "class",
+   "uriTo": "http://w3id.org/geo-lod/smoothingMethod_savgol"
+  },
+  {
+   "source": 1,
+   "target": 7,
+   "valueTo": "observedProperty",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/sosa/observedProperty"
+  },
+  {
+   "source": 1,
+   "target": 6,
+   "valueTo": "observedProperty",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/sosa/observedProperty"
+  },
+  {
+   "source": 0,
+   "target": 7,
+   "valueTo": "observedProperty",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/sosa/observedProperty"
+  },
+  {
+   "source": 0,
+   "target": 6,
+   "valueTo": "observedProperty",
+   "propertyTo": "class",
+   "uriTo": "http://www.w3.org/ns/sosa/observedProperty"
+  },
+  {
+   "source": 5,
+   "target": 2,
+   "valueTo": "collectedFrom",
+   "propertyTo": "class",
+   "uriTo": "http://w3id.org/geo-lod/collectedFrom"
+  },
+  {
+   "source": 5,
+   "target": 3,
+   "valueTo": "collectedFrom",
+   "propertyTo": "class",
+   "uriTo": "http://w3id.org/geo-lod/collectedFrom"
+  },
+  {
+   "source": 5,
+   "target": 4,
    "valueTo": "collectedFrom",
    "propertyTo": "class",
    "uriTo": "http://w3id.org/geo-lod/collectedFrom"
